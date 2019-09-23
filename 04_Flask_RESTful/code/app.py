64d267853df82303c28bf8c6179572fc2bb79449
Alejandro H Pineda <ale.hpineda@gmail.com>
2019-09-23 17:27:34 -0500

    Unit 04
    Creating item Resource

--- 04_Flask_RESTful/code/app.py
+++ 04_Flask_RESTful/code/app.py
@@ -5,11 +5,28 @@ app = Flask(__name__)
 
 api = Api(app)
 
-class Student(Resource):
+items = []
+
+class Item(Resource):
     def get(self, name):
-        return {'student': name}
+        # items is a list of dictionaries
+        for item in items:
+            # if the name in the dict equals the name of get
+            if item['name'] == name:
+                # return the dict
+                # we don't need jsonify, restful takes care of it
+                return item
+        return {'item': None}, 404
+
+    def post(self, name):
+        item = {
+            'name': name,
+            'price': 12.00
+        }
+        items.append(item)
+        return item, 201
 
 # as an api we don't need a decorator
-api.add_resource(Student, '/student/<string:name>')
+api.add_resource(Item, '/item/<string:name>')
 
 app.run(port=5000)
